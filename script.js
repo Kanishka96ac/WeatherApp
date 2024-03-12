82c5c0039defd4f0fce7c4c5f04b43eaffd14fb2
Kanishka96ac <ac.kanishka@gmail.com>
2024-03-12 01:28:06 +0000

    Fetch the API data

--- script.js
+++ script.js
@@ -2,11 +2,21 @@ console.log('JS Working...');
 
 
 const apiKey = "61d1dd7e50c5e2873a386472a54a7b2a";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=colombo"; 
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric"; 
 
-async function checkWeather(){
-    const response = await fetch(apiUrl+ `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl +`&appid=${apiKey}&q=${city}`);
     var data = await response.json();
 
-    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + '°C';
+    document.querySelector(".humidity").innerHTML = data.main.humidity + '%';
+    document.querySelector(".wind").innerHTML = data.wind.speed + ' km/h';
 }
+
+searchBtn.addEventListener("click", () =>{
+    checkWeather(searchBox.value);
+});
